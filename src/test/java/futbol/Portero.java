6e4567cf50cf60e0e53b1358d3626b8cb43251a3
Salomé Murillo Gaviria <samurillog@unal.edu.co>
2024-08-17 15:21:34 -0500

    Jugador y Portero

---
+++ src/test/java/futbol/Portero.java
@@ -0,0 +1,30 @@
+package futbol;
+
+public class Portero extends Futbolista{
+	
+	public short golesRecibidos;
+	public byte dorsal;
+	
+	public Portero(String nombre, int edad, String posicion, short golesRecibidos, byte dorsal) {
+		super(nombre, edad, "Portero");
+		this.golesRecibidos = golesRecibidos;
+		this.dorsal = dorsal;
+	}
+	
+	@Override
+	public int compareTo(Object f) {
+		Portero otroPortero = (Portero) f;
+		return Integer.compare(this.golesRecibidos, otroPortero.golesRecibidos);
+	}
+	
+	@Override
+	public boolean jugarConLasManos() {
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "El futbolista " + super.getNombre() + " tiene " + super.getEdad() + ", y juega de " + super.getPosicion() + " con el dorsal " + this.dorsal + ". Le han marcado " + this.golesRecibidos;
+	}
+	
+}
